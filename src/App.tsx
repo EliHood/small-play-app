f169ff21f48fdebb2887b5f6089d8fb23a0a7936
EliHood <lyrianapps@gmail.com>
2024-11-24 21:32:01 -0500

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import { Button, AppText, ImageCards } from "./components";
 
-console.log("process", import.meta.env);
-
 const isProduction = import.meta.env.PROD;
 
 function App() {
